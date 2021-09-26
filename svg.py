da610b37d4adf8e225f987b862cbb16a9ed0103e
huideyeren <takakura.yusuke@gmail.com>
2021-09-27 08:55:39 +0900

    naosu

--- svg.py
+++ svg.py
@@ -8,8 +8,9 @@ from reportlab.graphics import renderPDF
 
 
 def rotate(path):
-    svg_text = open(path, 'r', encoding='UTF-8')
-    svg = svgutils.transform.fromstring(svg_text)
+    svg_file = open(path, 'r', encoding='UTF-8')
+    svg_text = svg_file.read()
+    svg = svgutils.transform.fromstring(svg_text.encode('utf-8'))
     svg.rotate(-90)
     figure = svgutils.compose.Figure(svg.height, svg.width, svg)
     figure.save(path)
