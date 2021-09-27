960ad875fa630584569467707e2a58ff588d4720
huideyeren <takakura.yusuke@gmail.com>
2021-09-28 04:42:14 +0900

    update

--- plantuml.sh
+++ plantuml.sh
@@ -8,6 +8,13 @@ files=`find $pu_dir -maxdepth 0 -type f -name *.pu`
 
 for file in $files;
 do
-    echo $file
+    echo "${file}を変換しています。"
     java -jar /java/plantuml.jar -o $image_dir -tsvg $file
+done
+
+svgs=`find "${pu_dir}/*" -maxdepth 0 -type f -name *.svg`
+for svg in $svgs;
+do
+    echo "${svg}の内容は以下の通りです。"
+    cat $svg
 done
